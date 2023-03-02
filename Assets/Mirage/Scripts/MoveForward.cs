e5e003ad6b45e381b9761260c73b78e45748f59a
MirageEmil <miragellis4@gmail.com>
2023-03-02 13:55:43 -0800

    Second Edit
    I made a new player and added it. It moves and can shoot. I made a bullet and added it. It moves forward. I started to script a boundary script.

---
+++ Assets/Mirage/Scripts/MoveForward.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+}
